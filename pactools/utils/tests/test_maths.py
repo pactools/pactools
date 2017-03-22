94215db27aa3a79948cdc53b96b2593eee5c0ea4
Tom Dupré la Tour <tom.dupre-la-tour@m4x.org>
2017-03-22 18:22:08 +0100

    more maths tests

--- pactools/utils/tests/test_maths.py
+++ pactools/utils/tests/test_maths.py
@@ -1,11 +1,10 @@
-from functools import partial
-
 import numpy as np
 
-from pactools.utils.maths import norm, squared_norm, argmax_2d
+from pactools.utils.maths import norm, squared_norm, argmax_2d, is_power2
+from pactools.utils.maths import prime_factors
 from pactools.utils.testing import assert_equal
-from pactools.utils.testing import assert_raises, assert_array_equal
-from pactools.utils.testing import assert_true, assert_array_almost_equal
+from pactools.utils.testing import assert_array_almost_equal
+from pactools.utils.testing import assert_true, assert_false
 
 
 def test_norm():
@@ -16,8 +15,21 @@ def test_norm():
 
 
 def test_argmax_2d():
-    # Tet that argmax_2d gives the correct indices of the max
+    # Test that argmax_2d gives the correct indices of the max
     rng = np.random.RandomState(0)
     a = rng.randn(4, 3)
     i, j = argmax_2d(a)
     assert_equal(a[i, j], a.max())
+
+
+def test_is_power2():
+    for i in range(1, 10):
+        assert_true(is_power2(2 ** i))
+        assert_false(is_power2(2 ** i + 1))
+
+
+def test_prime_factors():
+    # Test that the prime factor decomposition products to the input integer
+    for n in range(3, 200, 2):
+        factors = prime_factors(n)
+        assert_equal(np.product(factors), n)
