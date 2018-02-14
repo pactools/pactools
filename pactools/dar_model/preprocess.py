Fix identical gap filling noise over epochs, and add option to average PSD in Spectrum

pactools/dar_model/preprocess.py
@@ -558,11 +558,6 @@ def multiple_extract_driver(sigs, fs, frequency_range, n_cycles=None,
         sigs = [whiten(sig, fs=fs, ordar=ordar, draw=draw) for sig in sigs]
         _show_plot(draw)
 
-    if fill == 2:
-        random_noise = rng.randn(len(sigs[0]))
-    else:
-        random_noise = None
-
     # extract the high frequencies independently of the driver
     if max_low_fq is None:
         max_low_fq = max(frequency_range)
@@ -570,9 +565,10 @@ def multiple_extract_driver(sigs, fs, frequency_range, n_cycles=None,
     low_pass_width = bandwidth
     low_and_high = [
         extract_and_fill(sig, fs=fs, fc=fc_low_pass, bandwidth=low_pass_width,
-                         fill=fill, random_noise=random_noise, draw=draw,
-                         extract_complex=False, low_pass=True,
-                         random_state=random_state) for sig in sigs
+                         fill=fill, random_noise=None,
+                         draw=draw, extract_complex=False, low_pass=True,
+                         random_state=rng.randint(np.iinfo(np.int32).max))
+        for sig in sigs
     ]
     high_sigs = [both[1] for both in low_and_high]
 

pactools/utils/spectrum.py
@@ -74,7 +74,7 @@ class Spectrum(object):
 
         return fft_length, step
 
-    def periodogram(self, signals, hold=False):
+    def periodogram(self, signals, hold=False, mean_psd=False):
         """
         Computes the estimation (in dB) for each epoch in a signal
 
@@ -88,9 +88,12 @@ class Spectrum(object):
             estimations, else, the list is emptied and only the current
             estimation is stored.
 
+        mean_psd : boolean, default = False
+            If True, the PSD is the mean PSD over all epochs.
+
         Returns
         -------
-        psd : array, shape (n_epochs, n_freq)
+        psd : array, shape (n_epochs, n_freq) or (1, n_freq) if mean_psd
             Power spectrum estimated with a Welsh method on each epoch
             n_freq = fft_length // 2 + 1
         """
@@ -128,6 +131,9 @@ class Spectrum(object):
             else:
                 scale = 1.0 / count
             psd[i] *= scale
+
+        if mean_psd:
+            psd = np.mean(psd, axis=0)[None, :]
 
         if not hold:
             self.psd = []